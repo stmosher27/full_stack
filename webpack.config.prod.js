9b3cbb0c4f667bb4d1ee7c1636c1443a17b12be7
stmosher27 <stmosher27@gmail.com>
2017-01-07 20:46:18 -0800

    first commit

---
+++ webpack.config.prod.js
@@ -0,0 +1,37 @@
+// let webpack = require("webpack");
+//
+// module.exports = {
+//   entry: './frontend/trail_gram.jsx',
+//   output: {
+//     path: 'app/assets/javascripts',
+//     filename: 'bundle.js',
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: [/\.jsx?$/, /\.js?$/],
+//         exclude: /(node_modules)/,
+//         loader: 'babel',
+//         query: {
+//           presets: ['es2015', 'react']
+//         }
+//       }
+//     ]
+//   },
+//   devtool: 'source-map',
+//   resolve: {
+//     extensions: ['', '.js', '.jsx' ]
+//   },
+//   plugins:[
+//     new webpack.DefinePlugin({
+//       'process.env':{
+//         'NODE_ENV': JSON.stringify('production')
+//       }
+//     }),
+//     new webpack.optimize.UglifyJsPlugin({
+//       compress:{
+//         warnings: true
+//       }
+//     })
+//   ]
+// };
