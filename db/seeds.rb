8ceb2caff1aaf299ec5e50ec0de2b5894b961927
stmosher27 <stmosher27@gmail.com>
2017-01-11 15:08:46 -0800

    Add guest seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create({username: 'guest', email: 'guest.com', name: 'John Smith', password: 'password'});
